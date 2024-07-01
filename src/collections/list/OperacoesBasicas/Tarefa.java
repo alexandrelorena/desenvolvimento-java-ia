ac342ce1c4d1d2b8eafc7329f90ff5e6a3353373
Alexandre Lorena <alexandre.lorena@gmail.com>
2024-06-30 21:40:30 -0300

    Atualizações

---
+++ src/collections/list/OperacoesBasicas/Tarefa.java
@@ -0,0 +1,14 @@
+package collections.list.OperacoesBasicas;
+
+public class Tarefa {
+    //atributo
+    private String descricao;
+
+    public Tarefa(String descricao) {
+        this.descricao = descricao; //construtor
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+}
