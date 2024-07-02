7368a50f6bf4601184c5db2164d9e606724632da
Alexandre Lorena <alexandre.lorena@gmail.com>
2024-07-01 21:07:34 -0300

    Atualizações

--- src/collections/list/OperacoesBasicas/Tarefa.java
+++ src/collections/list/OperacoesBasicas/Tarefa.java
@@ -11,4 +11,12 @@ public class Tarefa {
     public String getDescricao() {
         return descricao;
     }
+
+    @Override
+    public String toString() {
+//        return "Tarefa{" +
+//                "descricao='" + descricao + '\'' +
+//                '}';
+        return descricao;
+    }
 }
