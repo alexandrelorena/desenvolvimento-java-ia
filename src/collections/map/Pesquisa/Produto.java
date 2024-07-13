64e67cb21cfa1ab3afb8a1c5a031ad9369f4061d
Alexandre Lorena <alexandre.lorena@gmail.com>
2024-07-13 15:41:59 -0300

    atualização: Map > Pesquisa

---
+++ src/collections/map/Pesquisa/Produto.java
@@ -0,0 +1,37 @@
+package collections.map.Pesquisa;
+
+public class Produto {
+    //atributos
+    private String nome;
+    private double preco;
+    private int quantidade;
+
+    //construtor
+    public Produto(String nome, double preco, int quantidade) {
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    //getters
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" +
+                "nome='" + nome + '\'' +
+                ", preco=" + preco +
+                ", quantidade=" + quantidade +
+                '}';
+    }
+}
